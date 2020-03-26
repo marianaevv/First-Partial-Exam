a334f2b1baec7c5805001ee81de1fa529f5cc0c1
Marianavv <mariana2v@hotmail.com>
2020-03-26 09:01:44 -0600

    Exam

---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,10 @@
+let input = document.getElementsByClassName("userName");
+let btnAddCom = document.getElementById("submit");
+
+
+
+function displayName(){
+    document.getElementById("userComment") = input;
+}
+
+btnAddCom.onclick().value = displayName;
